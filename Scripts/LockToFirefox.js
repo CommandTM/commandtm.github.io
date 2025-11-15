d9b1141d5ec876a81aceb70f6b53ef084139a3a7
CommandTM <commandnoobmc23@gmail.com>
2025-11-15 00:57:29 -0500

    Please Fix My Script

--- Scripts/LockToFirefox.js
+++ Scripts/LockToFirefox.js
@@ -4,7 +4,7 @@ const lockHTML = '<div class="center" style="position: absolute; height: 100%; w
 
 console.log(window.navigator.userAgent)
 
-if (window.navigator.userAgent.includes('Chrome') || window.navigator.userAgent.includes('AppleWebKit')) {
+if ((window.navigator.userAgent.includes('Chrome') || window.navigator.userAgent.includes('AppleWebKit')) && !window.navigator.userAgent.includes('Firefox')) {
     console.log("KICK THEM OUT!!!")
     document.getElementsByTagName("body")[0].innerHTML += lockHTML;
     document.getElementsByTagName("body")[0].style.margin = "0";
