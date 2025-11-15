8fe9d2cf3869c8ca2fd978fa0b41e46284b454f0
CommandTM <commandnoobmc23@gmail.com>
2025-11-15 00:48:33 -0500

    Maybe The Firefox Lock Exists

---
+++ Scripts/LockToFirefox.js
@@ -0,0 +1,11 @@
+const lockHTML = '<div class="center" style="position: absolute; height: 100%; width: 100%; top: 0; left: 0; background-color: white;">\n' +
+    '    <img src="https://spu.edu/users/lauw/Sage/pic/best-viewed-in-firefox.jpg" />\n' +
+    '</div>'
+
+console.log(window.navigator.userAgent)
+
+if (!window.navigator.userAgent.includes('Mozilla')) {
+    console.log("KICK THEM OUT!!!")
+    document.getElementsByTagName("body")[0].innerHTML += lockHTML;
+    document.getElementsByTagName("body")[0].style.margin = "0";
+}
