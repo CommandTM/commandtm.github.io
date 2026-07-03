2a8321685774ba8c50dc76b78e01a0000572eeb7
CommandTM <commandnoobmc23@gmail.com>
2026-07-03 13:24:10 -0400

    Add The 2 Other Pronouns I Can Think Of To PronounRandomizer.js

--- Scripts/PronounRandomizer.js
+++ Scripts/PronounRandomizer.js
@@ -17,3 +17,23 @@ for (let her_his of her_his_spans) {
         her_his.innerHTML = her_his.classList.contains("capital")? "His" : "his"
     }
 }
+
+let she_he_spans = document.getElementsByClassName("she-he")
+
+for (let she_he of she_he_spans) {
+    if (Math.random() > 0.49) {
+        she_he.innerHTML = she_he.classList.contains("capital")? "She" : "she"
+    } else {
+        she_he.innerHTML = she_he.classList.contains("capital")? "He" : "he"
+    }
+}
+
+let her_him_spans = document.getElementsByClassName("her-him")
+
+for (let her_him of her_him_spans) {
+    if (Math.random() > 0.49) {
+        her_him.innerHTML = her_him.classList.contains("capital")? "Her" : "her"
+    } else {
+        her_him.innerHTML = her_him.classList.contains("capital")? "Him" : "him"
+    }
+}
